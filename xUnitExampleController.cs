92e2bea1733559aed67e42b4f93b6d2043e728c9
Anish Kumar Chaudhary <134726737+Anishkusmi@users.noreply.github.com>
2024-03-21 00:36:49 +0545

    Add files via upload

---
+++ xUnitExampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xUnitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class xUnitExampleController : ControllerBase
+    {
+        public string Index(int guessedNumber)
+        {
+            //return "I am in xUnitExample";
+            if(guessedNumber < 100)
+            {
+                return "Wrong! Try a Bigger number";
+            }
+            else if(guessedNumber > 100)
+            {
+                return "Wrong! Try a Smaller number";
+            }
+            else
+            {
+                return "You Gussed Correct number";
+            }
+        }
+    }
+}
